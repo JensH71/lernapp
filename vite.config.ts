ae660948b8cc8f24bfcc49e136c7b5736e57179a
JensH71 <jenshetzel@web.de>
2026-07-14 16:00:32 +0200

    Update Vite configuration for PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react() /* , VitePWA(...) */],
-  base: "https://JensH71.github.io/lernapp/"
-})
+  plugins: [
+    react(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
+  base: "/lernapp/",
+});
